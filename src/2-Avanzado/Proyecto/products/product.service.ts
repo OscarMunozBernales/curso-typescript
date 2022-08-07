90a260507071ff578f38cc53f0ab2652a45d09ba
Oscar Munoz Bernales <munoz.bernales.oscar@gmail.com>
2022-08-07 16:02:28 -0400

    Agregando DTO

--- src/2-Avanzado/Proyecto/products/product.service.ts
+++ src/2-Avanzado/Proyecto/products/product.service.ts
@@ -1,11 +1,24 @@
+import { faker } from '@faker-js/faker';
+import { CreateProductDto } from './product.dto';
 import { Product } from './product.model';
 
 export const products: Product[] = [];
 
-export const addProduct = (data: Product) => {
-  // data.id = 'hksjdhfskdfh';
-  // data.createdAt = new Date(1998, 1, 1);
-  products.push(data);
+export const addProduct = (data: CreateProductDto) => {
+  const newProduct = {
+    ...data,
+    id: faker.datatype.uuid(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    category: {
+      id: data.categoryId,
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    }
+  }
+  products.push(newProduct);
+  return newProduct;
 }
 
 
